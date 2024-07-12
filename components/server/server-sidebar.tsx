001e2cb90dbb57e73c3f645b49b324b31911b6e4
Suyun_Liang <suyunliang1@gmail.com>
2024-07-12 17:15:52 +0200

    added server channels and members in server section

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -10,6 +10,8 @@ import { ServerHeader } from "@/components/server/server-header";
 import { Separator } from "@/components/ui/separator";
 import { ServerSearch } from "./server-search";
 import { ServerSection } from "./sever-section";
+import { ServerChannel } from "./server-channel";
+import { ServerMember } from "./server-member";
 
 interface ServerSidebarProps {
   serverId: string;
@@ -116,6 +118,71 @@ export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
               role={role}
               label="Text Channels"
             />
+            <div className="space-y-[2px]">
+              {textChannels?.map((channel) => (
+                <ServerChannel
+                  key={channel.id}
+                  channel={channel}
+                  role={role}
+                  server={server}
+                />
+              ))}
+            </div>
+          </div>
+        )}
+        {Boolean(audioChannels?.length) && (
+          <div className="mb-2">
+            <ServerSection
+              sectionType="channels"
+              channelType={ChannelType.AUDIO}
+              role={role}
+              label="Voice Channels"
+            />
+            <div className="space-y-[2px]">
+              {audioChannels?.map((channel) => (
+                <ServerChannel
+                  key={channel.id}
+                  channel={channel}
+                  role={role}
+                  server={server}
+                />
+              ))}
+            </div>
+          </div>
+        )}
+        {Boolean(videoChannels?.length) && (
+          <div className="mb-2">
+            <ServerSection
+              sectionType="channels"
+              channelType={ChannelType.VIDEO}
+              role={role}
+              label="Video Channels"
+            />
+            <div className="space-y-[2px]">
+              {videoChannels?.map((channel) => (
+                <ServerChannel
+                  key={channel.id}
+                  channel={channel}
+                  role={role}
+                  server={server}
+                />
+              ))}
+            </div>
+          </div>
+        )}
+        {Boolean(members?.length) && (
+          <div className="mb-2">
+            <ServerSection
+              sectionType="members"
+              role={role}
+              label="Members"
+              server={server}
+            />
+            <div className="space-y-[2px]">
+              {members?.map((member) => (
+                <ServerMember key={member.id} server={server} member={member} />
+              ))}
+            </div>
           </div>
         )}
       </ScrollArea>
