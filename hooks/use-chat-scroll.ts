abb1b6e36d6d05317f15360803e55a78282265f7
Suyun Liang <suyunliang@Suyuns-Air.fritz.box>
2024-08-07 15:18:51 +0200

    added use-chat-scroll hooks

---
+++ hooks/use-chat-scroll.ts
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+
+type chatScrollProps = {
+  chatRef: React.RefObject<HTMLDivElement>;
+  bottomRef: React.RefObject<HTMLDivElement>;
+  shouldLoadMore: boolean;
+  loadMore: () => void;
+  count: number;
+};
+
+export const useChatScroll = ({
+  chatRef,
+  bottomRef,
+  shouldLoadMore,
+  loadMore,
+  count,
+}: chatScrollProps) => {
+  const [hasInitialized, setHasInitialized] = useState(false);
+
+  useEffect(() => {
+    const topDiv = chatRef?.current;
+
+    const handleScroll = () => {
+      const scrollTop = topDiv?.scrollTop;
+      if (scrollTop === 0 && shouldLoadMore) {
+        loadMore();
+      }
+    };
+    topDiv?.addEventListener("scroll", handleScroll);
+
+    return () => {
+      removeEventListener("scroll", handleScroll);
+    };
+  }, [shouldLoadMore, loadMore, chatRef]);
+
+  //scroll to the bottom if there is a new message
+  useEffect(() => {
+    const bottomDiv = bottomRef?.current;
+    const topDiv = chatRef?.current;
+    const shouldAutoScroll = () => {
+      if (!hasInitialized && bottomDiv) {
+        setHasInitialized(true);
+        return true;
+      }
+      if (!topDiv) return false;
+
+      const distanceFromBottom =
+        topDiv.scrollHeight - topDiv.scrollTop - topDiv.clientHeight;
+
+      return distanceFromBottom <= 100;
+    };
+    if (shouldAutoScroll()) {
+      setTimeout(() => {
+        bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+      }, 100);
+    }
+  }, [bottomRef, chatRef, hasInitialized, count]);
+};
